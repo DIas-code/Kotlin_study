6e2573081452982bc77e1e12a9b5ffc287b26494
Yermekov Dias <diasermekov09@gmail.com>
2025-03-03 00:29:24 +0500

    OOP

--- OOP/src/main/kotlin/Classes.kt
+++ OOP/src/main/kotlin/Classes.kt
@@ -8,3 +8,23 @@ class Car(brand: String, var model: String, var color: String, var doors: Int) {
         println("The car $brand is stopping")
     }
 }
+
+class User(var name: String, var lastname: String, var age: Int) {
+    init {
+        println("User: $name was created")
+    }
+}
+
+class Calculator() {
+    companion object{
+        fun sum (a: Int, b:Int): Int{
+            return a+b
+        }
+    }
+}
+
+object Database {
+    init {
+        println("DB created")
+    }
+}
--- OOP/src/main/kotlin/Main.kt
+++ OOP/src/main/kotlin/Main.kt
@@ -1,8 +1,21 @@
 fun main() {
-    val user = User("Alex", "Zubenko", 23)
+    val direction = Direction.valueOf("east".uppercase())
+
+    when(direction) {
+        Direction.EAST -> println("The direction is East")
+        Direction.NORTH -> println("The direction is NORTH")
+        Direction.SOUTH -> println("The direction is SOUTH")
+        Direction.WEST -> println("The direction is WEST")
+    }
 }
 
-class User(var name: String, var lastname:String,
-    var age: Int) {
+enum class Direction(var direction: String, var distance: Int) {
+    NORTH("north", 10),
+    SOUTH("south", 20),
+    EAST("east", 15),
+    WEST("west", 40);
 
+    fun printData() {
+        println("Direction=$direction and Distance=$distance")
+    }
 }
