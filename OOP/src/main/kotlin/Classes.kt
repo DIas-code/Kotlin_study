c44a96cec1539cfe1e661808abe60266d71f84c1
Yermekov Dias <diasermekov09@gmail.com>
2025-03-03 14:05:07 +0500

    OOP

--- OOP/src/main/kotlin/Classes.kt
+++ OOP/src/main/kotlin/Classes.kt
@@ -28,3 +28,23 @@ object Database {
         println("DB created")
     }
 }
+
+
+enum class Direction(var direction: String, var distance: Int) {
+    NORTH("north", 10),
+    SOUTH("south", 20),
+    EAST("east", 15),
+    WEST("west", 40);
+
+    fun printData() {
+        println("Direction=$direction and Distance=$distance")
+    }
+}
+
+class ListView(val items: Array<String>) {
+    inner class ListViewItem() {
+        fun displayItem(position: Int) {
+            println(items[position])
+        }
+    }
+}
--- OOP/src/main/kotlin/Main.kt
+++ OOP/src/main/kotlin/Main.kt
@@ -1,21 +1,64 @@
 fun main() {
-    val direction = Direction.valueOf("east".uppercase())
+    val diasAccount = Account("Dias")
+    diasAccount.deposit(600)
+    diasAccount.deposit(-800)
+    diasAccount.withdraw(400)
+    diasAccount.withdraw(-233)
+    val asdAccount = Account("asd")
+    asdAccount.deposit(123)
+    asdAccount.withdraw(123)
+    asdAccount.deposit(12312)
+    println(asdAccount.calculateBalance())
+}
 
-    when(direction) {
-        Direction.EAST -> println("The direction is East")
-        Direction.NORTH -> println("The direction is NORTH")
-        Direction.SOUTH -> println("The direction is SOUTH")
-        Direction.WEST -> println("The direction is WEST")
+class Account(val accountName: String) {
+    private var balance: Int = 0
+    private var transactions = mutableListOf<Int>()
+
+    fun deposit(amount: Int) {
+        if (amount > 0) {
+            transactions.add(amount)
+            this.balance += amount
+            println("$amount was added. Balance now = $balance")
+        } else {
+            println("cannot deposit amount that equal or lower than 0")
+        }
+    }
+
+    fun withdraw(withdrawal: Int) {
+        if (-withdrawal < 0) {
+            transactions.add(-withdrawal)
+            this.balance += (-withdrawal)
+            println("$withdrawal was with. Balance now = $balance")
+        } else {
+            println("cannot withdraw negative sums")
+        }
+    }
+
+    fun calculateBalance(): Int {
+        this.balance = 0
+        for (transaction in transactions){
+            this.balance += transaction
+        }
+        return this.balance
     }
 }
 
-enum class Direction(var direction: String, var distance: Int) {
-    NORTH("north", 10),
-    SOUTH("south", 20),
-    EAST("east", 15),
-    WEST("west", 40);
 
-    fun printData() {
-        println("Direction=$direction and Distance=$distance")
-    }
-}
+//fun main() {
+//    val listView = ListView(arrayOf("name1", "name2", "name3", "name4"))
+//    listView.ListViewItem().displayItem(3)
+//}
+
+
+//fun main() {
+//    val direction = Direction.valueOf("east".uppercase())
+//
+//    when(direction) {
+//        Direction.EAST -> println("The direction is East")
+//        Direction.NORTH -> println("The direction is NORTH")
+//        Direction.SOUTH -> println("The direction is SOUTH")
+//        Direction.WEST -> println("The direction is WEST")
+//    }
+//}
+//
